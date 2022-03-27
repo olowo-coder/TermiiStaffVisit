db2f1effd5c1e75f6d68d2920f565ff6035530d5
olowo-coder <olowofela.olanrewaju@decagon.dev>
2022-03-27 19:50:36 +0100

    Initialization

---
+++ src/main/java/com/example/termiistaffvisitor/TermiiStaffVisitorApplication.java
@@ -0,0 +1,13 @@
+package com.example.termiistaffvisitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TermiiStaffVisitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TermiiStaffVisitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/termiistaffvisitor/TermiiStaffVisitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.termiistaffvisitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TermiiStaffVisitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
